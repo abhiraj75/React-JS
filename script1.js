6ac222661b5ec8e370b9240e18981c209f78effa
abhiraj75 <itsabhiraj27@gmail.com>
2026-01-28 11:06:55 +0530

    initializing repo

---
+++ script1.js
@@ -0,0 +1,43 @@
+// const root=document.querySelector("#root")
+// const element=React.createElement("h3",{clasName:"sem2",id:"abc"},"Hello React")
+// ReactDOM.render(element,root)
+
+// const ele= <p>Hello Abhiraj</p>
+// const ele2=<div>
+//     <p>Hello</p>
+//     <p>Welcome</p>
+// </div>
+
+// ReactDOM.render(ele,root)
+// ReactDOM.render(ele2,root)
+
+// function App(){
+//     return <p>Hello From React Component</p>
+// }
+
+// ReactDOM.render(App(),root)
+
+// const root=ReactDOM.createRoot(document.querySelector("#root"))
+// root.render(<App/>)
+
+
+
+// function App(props){
+//     return <p>Hello From React Component {props}</p>
+// }
+
+// const a=App("Abhiraj")
+// const root=ReactDOM.createRoot(document.querySelector("#root"))
+// root.render(a)
+
+
+function App(props){
+    return <div>
+        {props.books.map((ele)=>{
+            return <li style={{color:"red",background:"black"}}>{ele}</li>
+        })}
+    </div>
+}
+
+const root=ReactDOM.createRoot(document.querySelector("#root"))
+root.render(<App books={["javascript", "css", "html", "react"]}/>)

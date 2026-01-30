172e3be86e432b3adffd0eb06ee3ae1ecab28924
abhiraj75 <itsabhiraj27@gmail.com>
2026-01-30 11:34:23 +0530

    Modifying Users Project

--- users/src/component/Profile.jsx
+++ users/src/component/Profile.jsx
@@ -1,6 +1,6 @@
 import {users} from "../users"
 import {useState} from "react"
-
+import "./Profile.css"
 
 function Profile(){
     const [allUsers,setAllUsers]=useState(users)
@@ -14,9 +14,25 @@ function Profile(){
             setAllUsers(updatedLikes)
     }
 
-    return <div>
+    return <div id="root">
+        <h2><u>Seniors:</u></h2>  
         {allUsers.map((val)=>{
-            return <div key={val.id}>
+            if (val.experience>=4){
+            return <div key={val.id} id="card">
+                <img src={val.image}></img>
+                <h3>{val.name}</h3>
+                <p>{val.description}</p>
+                <p>{val.experience>=4?"Senior":"Fresher"}</p>
+                <p onClick={()=>increaseLikes(val.id)}>Likes : {val.likes}</p>
+                <hr></hr>
+                </div>
+                
+            }return null
+            })}
+        <h2><u>Freshers:</u></h2>   
+        {allUsers.map((val)=>{
+            if (val.experience<4){
+            return <div key={val.id} id="card">
                 <img src={val.image}></img>
                 <h3>{val.name}</h3>
                 <p>{val.description}</p>
@@ -24,7 +40,9 @@ function Profile(){
                 <p onClick={()=>increaseLikes(val.id)}>Likes : {val.likes}</p>
                 <hr></hr>
                 </div>
-        })}
+            }return null
+            })}
+           
     </div>
 }
 

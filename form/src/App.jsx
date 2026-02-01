3b257d2bcde4f6f2d98018cada287872e584ca17
abhiraj75 <itsabhiraj27@gmail.com>
2026-02-01 11:42:14 +0530

    complete forms

--- form/src/App.jsx
+++ form/src/App.jsx
@@ -6,25 +6,21 @@ function App() {
   const [toggle,setToggle]=useState("")
   function showForm(e){
     setToggle(e.target.value)
-
   }
 
   return (
     <div id="root">
-      <form style={{display:toggle==="signup"?"block":"none"}}>
-        <input type="text" placeholder="Name"></input>
+      <form style={{display:toggle?"block":"none"}}>
+        {toggle==="signup" && <input type="text" placeholder="Name"></input>}
         <input type="email" placeholder='Email'></input>
-        <input type="date" placeholder='Date'></input>
-        <input type="text" placeholder='Password'></input>
+        {toggle==="signup" && <input type="date" placeholder='Date'></input>}
+        <input type="text" placeholder='Password'></input><br></br>
+        {toggle==="login" &&<button style={{backgroundColor:"red"}}>Login</button>}
+        {toggle==="signup" &&<button style={{backgroundColor:"red"}}>Sign Up</button>}
       </form>
-      <form style={{display:toggle==="login"?"block":"none"}}>
-        <input type="email" placeholder='Email'></input>
-        <input type="text" placeholder='Password'></input>
 
-      </form>
-     
-      <button value="login"onClick={(e)=>showForm(e)}>Login</button>
-      <button value="signup"onClick={(e)=>showForm(e)}>Sign Up</button>
+      {!toggle &&<button value="login"onClick={(e)=>showForm(e)}>Login</button>}
+      {!toggle &&<button value="signup"onClick={(e)=>showForm(e)}>Sign Up</button>}
     </div>
   ) 
 }

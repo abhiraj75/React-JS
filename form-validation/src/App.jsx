b35f96cdbe316a8cf6cf2b446b04460a01bfd9d0
abhiraj75 <itsabhiraj27@gmail.com>
2026-02-04 12:18:20 +0530

    completed form validation

--- form-validation/src/App.jsx
+++ form-validation/src/App.jsx
@@ -1,34 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {toast} from "react-toastify"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState({
+    name:"",
+    email:"",
+    number:"",
+    password:""
+  })
+
+  const[error,setError]=useState(null)
+  function handleSubmit(e){
+    e.preventDefault();
+    if (user.name.length<3) toast.error("Invalid Name")
+    if (!user.email.includes("@") || (!user.email.includes("@"))) toast.error("Invalid Email")
+    if (user.number.length!=10) toast.error("Invalid Number")
+    if (user.password.length<8) toast.error("Weak Password")
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Name" onChange={(e)=>setUser({...user,name:e.target.value})}></input><br></br>
+        <input type="text" placeholder="Email" onChange={(e)=>setUser({...user,email:e.target.value})}></input><br></br>
+        <input type="text" placeholder="Mobile Number" onChange={(e)=>setUser({...user,number:e.target.value})}></input><br></br>
+        <input type="text" placeholder="Password" onChange={(e)=>setUser({...user,password:e.target.value})}></input><br></br>
+        <input id="sub" type="Submit"></input>
+      </form>
+    </div>
   )
 }
 
